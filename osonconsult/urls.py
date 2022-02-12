3eb04eb2cd074ed9a5c7fb845fed440dcc3f0639
Olukunle <ogundeji.olukunle@gmail.com>
2022-02-12 01:40:21 +0100

    update

--- osonconsult/urls.py
+++ osonconsult/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('', include('blogpage.urls')),
-    path('', include('program.urls')),
+    path('', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    #path('', include('home.urls')),
+    #path('', include('blogpage.urls')),
+    #path('', include('program.urls')),
 
 ]
